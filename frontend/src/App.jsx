da58419bdb83038e62f73311b8961bdce37f6434
Burkhon Nurmurodov <burkhon0207@gmail.com>
2026-06-26 15:08:49 +0500

    auto: update App.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -263,6 +263,7 @@ function AppWithLang() {
             <Route path="/production" element={<AuthGate><RequirePage page="production"><Production /></RequirePage></AuthGate>} />
             <Route path="/trudoyomkost" element={<AuthGate><RequirePage page="trudoyomkost"><Trudoyomkost /></RequirePage></AuthGate>} />
             <Route path="/leaders" element={<AuthGate><RequirePage page="leaders"><Leaders /></RequirePage></AuthGate>} />
+            <Route path="/kaizen" element={<AuthGate><RequirePage page="kaizen"><Kaizen /></RequirePage></AuthGate>} />
             <Route
               path="/admin/upload"
               element={<AuthGate><RequireAdmin><AdminUpload /></RequireAdmin></AuthGate>}
