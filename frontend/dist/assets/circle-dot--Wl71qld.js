7686eed90a7253604819d3023b2e78e762a7656a
Burkhon Nurmurodov <burkhon0207@gmail.com>
2026-07-03 18:53:38 +0500

    auto: update translations.js

--- frontend/dist/assets/circle-dot--Wl71qld.js
+++
@@ -1,1 +0,0 @@
-import{m as c}from"./Skeleton-CvANVZ1K.js";var r=[["circle",{cx:"12",cy:"12",r:"10",key:"1mglay"}],["circle",{cx:"12",cy:"12",r:"1",key:"41hilf"}]],i=c("circle-dot",r);export{i as t};
