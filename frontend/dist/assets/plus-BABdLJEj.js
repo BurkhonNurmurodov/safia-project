625cc88a7a9b111bf618da9b3d7f33b1a079a0e1
Burkhon Nurmurodov <burkhon0207@gmail.com>
2026-07-03 18:52:45 +0500

    auto: update translations.js

--- frontend/dist/assets/plus-BABdLJEj.js
+++
@@ -1,1 +0,0 @@
-import{m as a}from"./Skeleton-B4H0w1Ei.js";var e=[["path",{d:"M5 12h14",key:"1ays0h"}],["path",{d:"M12 5v14",key:"s699le"}]],r=a("plus",e);export{r as t};
---
+++ frontend/dist/assets/user-round-CoMTekfr.js
@@ -0,0 +1,1 @@
+import{m as r}from"./Skeleton-xFIxMk0T.js";var e=[["circle",{cx:"12",cy:"8",r:"5",key:"1hypcn"}],["path",{d:"M20 21a8 8 0 0 0-16 0",key:"rfgkzh"}]],o=r("user-round",e);export{o as t};
