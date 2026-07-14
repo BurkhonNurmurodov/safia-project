a114ce4243a1565427b54412598ac21c17abff84
Burkhon Nurmurodov <burkhon0207@gmail.com>
2026-07-14 12:18:56 +0500

    auto: update translations.js

--- frontend/dist/assets/circle-dot-CeM_RB2P.js
+++
@@ -1,1 +0,0 @@
-import{t as c}from"./createLucideIcon-CYal1gl3.js";var r=[["circle",{cx:"12",cy:"12",r:"10",key:"1mglay"}],["circle",{cx:"12",cy:"12",r:"1",key:"41hilf"}]],i=c("circle-dot",r);export{i as t};
