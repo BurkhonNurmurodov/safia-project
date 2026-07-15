543783c1017511f15c433320990b7ccfc17dce4c
Burkhon Nurmurodov <burkhon0207@gmail.com>
2026-07-15 18:09:51 +0500

    auto: update translations.js

--- frontend/dist/assets/chevron-down-Bi8iBUtM.js
+++
@@ -1,1 +0,0 @@
-import{t as o}from"./createLucideIcon-D88SI8S4.js";var r=[["path",{d:"m6 9 6 6 6-6",key:"qrunsl"}]],n=o("chevron-down",r);export{n as t};
--- frontend/dist/assets/minus-g0JxhNKV.js
+++
@@ -1,1 +0,0 @@
-import{t as a}from"./createLucideIcon-D88SI8S4.js";var e=[["path",{d:"M5 12h14",key:"1ays0h"}]],r=a("minus",e);export{r as t};
