751d6db40eb399d2a169dc2849bd674b98aafb2c
Burkhon Nurmurodov <burkhon0207@gmail.com>
2026-07-16 15:53:06 +0500

    auto: update translations.js

--- frontend/dist/assets/chevron-down-D1FIiGkB.js
+++
@@ -1,1 +0,0 @@
-import{i as o}from"./transliterate-2UxvHDjS.js";var r=[["path",{d:"m6 9 6 6 6-6",key:"qrunsl"}]],n=o("chevron-down",r);export{n as t};
--- frontend/dist/assets/circle-dot-CutppUnx.js
+++
@@ -1,1 +0,0 @@
-import{i as c}from"./transliterate-2UxvHDjS.js";var r=[["circle",{cx:"12",cy:"12",r:"10",key:"1mglay"}],["circle",{cx:"12",cy:"12",r:"1",key:"41hilf"}]],i=c("circle-dot",r);export{i as t};
