ce04b750e045dd76e2380f1d166284546d8f4a32
Burkhon Nurmurodov <burkhon0207@gmail.com>
2026-07-21 20:34:57 +0500

    auto: update translations.js

--- frontend/dist/assets/user-round-Dy4KWaKt.js
+++
@@ -1,1 +0,0 @@
-import{i as r}from"./transliterate-D_Aroh0b.js";var e=[["circle",{cx:"12",cy:"8",r:"5",key:"1hypcn"}],["path",{d:"M20 21a8 8 0 0 0-16 0",key:"rfgkzh"}]],o=r("user-round",e);export{o as t};
