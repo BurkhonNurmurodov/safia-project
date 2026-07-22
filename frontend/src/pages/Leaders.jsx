966ad37e498014f3c54e2addb97698a1ed2059cb
Burkhon Nurmurodov <burkhon0207@gmail.com>
2026-07-22 14:27:12 +0500

    auto: update Leaders.jsx

--- frontend/src/pages/Leaders.jsx
+++ frontend/src/pages/Leaders.jsx
@@ -1192,7 +1192,7 @@ export default function Leaders({ botMode = false }) {
                 <tr>
                   <Th label={T.thPlace} cls="w-[58px]" />
                   <Th label={effStandMode === "leader" ? T.thLeader : T.supervisor} cls="border-l border-[var(--border)]" />
-                  <Th label={T.thDays} cls="border-l border-[var(--border)] w-[92px]" hint={`${T.daysSent} – ${T.daysMissed}`} />
+                  <Th label={T.thDays} cls="border-l border-[var(--border)] w-[132px]" hint={`${T.daysSent} / ${T.winLabel}`} />
                   <Th label={T.standRating} k="rating" sort={standSort} onSort={onStandSort} cls="border-l border-[var(--border)] w-[104px]" />
                   <Th label={T.standConsist} k="consist" sort={standSort} onSort={onStandSort} cls="border-l border-[var(--border)] w-[112px]" />
                   <Th label={T.thTier} cls="border-l border-[var(--border)] w-[116px]" />
