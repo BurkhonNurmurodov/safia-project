eafd769fac101aef44301fc6a5d0cddc25611679
Burkhon Nurmurodov <burkhon0207@gmail.com>
2026-07-23 16:19:05 +0500

    auto: update translations.js

---
+++ frontend/dist/assets/chevron-left-CThPoXSK.js
@@ -0,0 +1,1 @@
+import{i as e}from"./transliterate-BKS8i5o7.js";var o=[["path",{d:"m15 18-6-6 6-6",key:"1wnfg3"}]],t=e("chevron-left",o);export{t};
---
+++ frontend/dist/assets/minus-Bix2bfyw.js
@@ -0,0 +1,1 @@
+import{i as a}from"./transliterate-BKS8i5o7.js";var e=[["path",{d:"M5 12h14",key:"1ays0h"}]],o=a("minus",e);export{o as t};
