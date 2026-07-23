2d4a2c4d65afc1d74f3c95ead5819bd28323dd12
Burkhon Nurmurodov <burkhon0207@gmail.com>
2026-07-23 12:58:57 +0500

    auto: update translations.js

---
+++ frontend/dist/assets/chevron-right-CBG92piY.js
@@ -0,0 +1,1 @@
+import{i as r}from"./transliterate-DLF2I6fb.js";var e=[["path",{d:"m9 18 6-6-6-6",key:"mthhwq"}]],t=r("chevron-right",e);export{t};
---
+++ frontend/dist/assets/plus-sZPLbDnq.js
@@ -0,0 +1,1 @@
+import{i as a}from"./transliterate-DLF2I6fb.js";var e=[["path",{d:"M5 12h14",key:"1ays0h"}],["path",{d:"M12 5v14",key:"s699le"}]],r=a("plus",e);export{r as t};
