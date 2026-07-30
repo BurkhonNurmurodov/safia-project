dcc86e4b3f0fd16f44e88cbade4f9e19522499a7
Burkhon Nurmurodov <burkhon0207@gmail.com>
2026-07-30 15:22:20 +0500

    auto: update App.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -61,6 +61,7 @@ const Tasks = lazyWithReload(() => import("./pages/Tasks"));
 const UsersActivity = lazyWithReload(() => import("./pages/UsersActivity"));
 const SetupTimes = lazyWithReload(() => import("./pages/SetupTimes"));
 const IdleCell = lazyWithReload(() => import("./pages/IdleCell"));
+const CellAttendance = lazyWithReload(() => import("./pages/CellAttendance"));
 const BroadcastReceivers = lazyWithReload(() => import("./pages/BroadcastReceivers"));
 const Login = lazyWithReload(() => import("./pages/Login"));
 import PageLoader from "./components/ui/PageLoader";
