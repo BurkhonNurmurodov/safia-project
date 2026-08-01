fea097f02a2d70e473236b13bdd3261786e21464
Burkhon Nurmurodov <burkhon0207@gmail.com>
2026-08-01 15:39:35 +0500

    auto: update passenger_wsgi.py

--- backend/passenger_wsgi.py
+++ backend/passenger_wsgi.py
@@ -53,6 +53,7 @@ try:
         migrate_cells_leaders_columns, migrate_cell_supervisor_column,
         migrate_cell_in_load_column,
         migrate_cell_ojidaniya_percat,
+        migrate_cell_perenaladka,
         migrate_attendance_batches,
         backfill_role_profiles,
         add_concern_profile_columns, add_concern_done_at, add_concern_level_columns,
