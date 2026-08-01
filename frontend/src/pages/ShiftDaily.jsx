dcfb8691b63760de10735b5fc2fd35bfbd88614c
Burkhon Nurmurodov <burkhon0207@gmail.com>
2026-08-01 10:55:44 +0500

    auto: update ShiftDaily.jsx

--- frontend/src/pages/ShiftDaily.jsx
+++ frontend/src/pages/ShiftDaily.jsx
@@ -13,6 +13,7 @@ import BrigadirTable from "../components/ui/BrigadirTable";
 import EmptyState from "../components/ui/EmptyState";
 import { SkeletonCard, SkeletonChart } from "../components/ui/Skeleton";
 import { useFilters } from "../context/FilterContext";
+import { usePersistentState } from "../hooks/usePersistentState";
 import { useLang } from "../context/LangContext";
 import { useTranslit } from "../utils/transliterate";
 import { fmtPct, fmtTime } from "../utils/formatters";
