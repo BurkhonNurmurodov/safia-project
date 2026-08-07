da0fb022d71f706e19a808fafe02934464c9a6fc
Burkhon Nurmurodov <burkhon0207@gmail.com>
2026-08-07 11:18:08 +0500

    auto: update Workers.jsx

--- frontend/src/pages/Workers.jsx
+++ frontend/src/pages/Workers.jsx
@@ -18,6 +18,8 @@ import HeatmapChart from "../components/charts/HeatmapChart";
 import { SkeletonCard, SkeletonChart } from "../components/ui/Skeleton";
 import { useFilters } from "../context/FilterContext";
 import { usePersistentState } from "../hooks/usePersistentState";
+import FactoryTabs from "../components/ui/FactoryTabs";
+import { useFactoryParams, useFactorySupervisors } from "../context/FactoryContext";
 import { useLang } from "../context/LangContext";
 import { useTranslit } from "../utils/transliterate";
 import { useChartTheme } from "../hooks/useChartTheme";
