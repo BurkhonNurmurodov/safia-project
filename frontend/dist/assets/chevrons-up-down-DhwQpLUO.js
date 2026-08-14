87c48d150e63a64e7c304a5f61da7c40b21a6bd5
Burkhon Nurmurodov <burkhon0207@gmail.com>
2026-08-14 15:49:49 +0500

    Update translations.js

--- frontend/dist/assets/chevrons-up-down-DhwQpLUO.js
+++
@@ -1,1 +0,0 @@
-import{t as o}from"./createLucideIcon-NQZWHa2t.js";var e=[["path",{d:"m7 15 5 5 5-5",key:"1hf1tw"}],["path",{d:"m7 9 5-5 5 5",key:"sgt6xg"}]],r=o("chevrons-up-down",e);export{r as t};
