0bc5ca351d1be2f8318df31b7e115b406a09b24b
Burkhon Nurmurodov <burkhon0207@gmail.com>
2026-09-23 11:29:48 +0500

    v4.148.2: Release v4.148.2

---
+++ frontend/dist/assets/useStatusBands-DemYB6vD.js
@@ -0,0 +1,1 @@
+import{t as e}from"./api-DMbpsrlR.js";import{G as r}from"./index-Q2rghCdK.js";import{a as n,n as o}from"./statusBands-1F25AwmR.js";function p(){const{data:t}=r({queryKey:["status-bands"],queryFn:()=>e.get("/api/status-bands").then(s=>{var a;return n((a=s.data)===null||a===void 0?void 0:a.bands)}),staleTime:3e5,refetchOnWindowFocus:!1});return t||o}export{p as t};
